c66ee05a733ca3619449371ac86f380a73d38559
Tyrone Maasdorp-Mazibuko <66698842+Tyrone17@users.noreply.github.com>
2022-10-07 04:54:53 +0200

    Update main.js
    Adding function to get quote

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 //ToDo: create variables to store quote and name as string
-var url = "https://codepen.io/Tyrone1730/pen/XWqPZwa" 
+var url = "https://docs.google.com/document/d/1ZYkbINPutWRcq-SwHVAXCJDklcpsPEnDi128K4Z3BSI/edit?usp=drivesdk"
+
+function get Quote () {
+ $.getJSON(url, function (resp {
+   $.each(resp, function (key, value) {
+      print(key + value) 
+} )
+}) 
+}
