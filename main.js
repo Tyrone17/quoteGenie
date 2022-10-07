f31e7d156eaf88fc1d080e3e4180821befe3f74a
Tyrone Maasdorp-Mazibuko <66698842+Tyrone17@users.noreply.github.com>
2022-10-07 04:55:53 +0200

    Update main.js
    Corrected function name

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //ToDo: create variables to store quote and name as string
 var url = "https://docs.google.com/document/d/1ZYkbINPutWRcq-SwHVAXCJDklcpsPEnDi128K4Z3BSI/edit?usp=drivesdk"
 
-function get Quote () {
+function getQuote () {
  $.getJSON(url, function (resp {
    $.each(resp, function (key, value) {
       print(key + value) 
